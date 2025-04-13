0d289266b3468caa0e96ec99a47c6972992abd18
Iskz17 <61486047+Iskz17@users.noreply.github.com>
2025-04-13 21:47:58 +0800

    font stuff related

--- src/theme.ts
+++ src/theme.ts
@@ -5,10 +5,32 @@ export const createAppTheme = (isDark: boolean): MD3Theme & {
   colors: MD3Theme['colors'] & { muted: string };
 } => {
   const base = isDark ? MD3DarkTheme : MD3LightTheme;
+  const applyFont = (style) => ({
+    ...style,
+    fontFamily: 'Gilroy',
+  });
 
   return {
     ...base,
     roundness: 8,
+    fonts: {
+      ...base.fonts,
+      displayLarge: applyFont(base.fonts.displayLarge),
+      displayMedium: applyFont(base.fonts.displayMedium),
+      displaySmall: applyFont(base.fonts.displaySmall),
+      headlineLarge: applyFont(base.fonts.headlineLarge),
+      headlineMedium: applyFont(base.fonts.headlineMedium),
+      headlineSmall: applyFont(base.fonts.headlineSmall),
+      titleLarge: applyFont(base.fonts.titleLarge),
+      titleMedium: applyFont(base.fonts.titleMedium),
+      titleSmall: applyFont(base.fonts.titleSmall),
+      bodyLarge: applyFont(base.fonts.bodyLarge),
+      bodyMedium: applyFont(base.fonts.bodyMedium),
+      bodySmall: applyFont(base.fonts.bodySmall),
+      labelLarge: applyFont(base.fonts.labelLarge),
+      labelMedium: applyFont(base.fonts.labelMedium),
+      labelSmall: applyFont(base.fonts.labelSmall),
+    },
     colors: {
       ...base.colors,
       primary: '#007AFF',
